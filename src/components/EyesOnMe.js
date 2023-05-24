0cd624c575d5a8e449df550b4924dd875560fe28
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-05-24 04:03:06 -0400

    complete lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+  function handleOnfocus() {
+    console.log("Good!");
+  }
+
+  function handleOnblur() {
+    console.log("Hey! Eyes on me!");
+  }
+
+  return (
+    <>
+      <button onBlur={handleOnblur} onFocus={handleOnfocus}>
+        Eyes on me
+      </button>
+    </>
+  );
+}
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,25 @@
 // Code Keypad Component Here
+import React, { useState } from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  const [value, setValue] = useState("");
+
+  function handleChange(e) {
+    setValue(e.value);
+    console.log("Entering password...");
+    console.log(value);
+  }
+
+  return (
+    <>
+      <input
+        type="password"
+        value={value}
+        name="password"
+        onChange={(e) => handleChange(e.target)}
+      />
+    </>
+  );
 }
 
 export default Keypad;
